d9a3e641194abc834d973cdd76a7d9cbad7f9847
Peter Anguelov <pet.anguelov@gmail.com>
2026-04-28 06:04:15 +0200

    Page Routes and List on Home page

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/post", (req, res) => {
+    res.render("post.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
