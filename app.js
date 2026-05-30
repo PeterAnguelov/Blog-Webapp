9c4a6725da9ee758200b59da7764074b1f671593
Peter Anguelov <pet.anguelov@gmail.com>
2026-05-30 18:04:59 +0200

    New post button added and now new post can be added to the posts JSON

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true}));
 
 // loads root or index page
 app.get("/", (req, res) => {
@@ -36,8 +37,41 @@ app.get("/post/:slug", (req, res) => {
 // loads new post page
 app.get("/new-post", (req, res) => {
     res.render("newPost.ejs");
-})
+});
 
+// this will create and add the new post to the posts JSON file
+app.post("/new-post", (req, res) => {
+    const posts = JSON.parse(fs.readFileSync('./data/posts.json', 'utf8'));
+
+    const newPost = {
+        id: posts.length + 1,
+        slug: slugGenerator(req.body.title),
+        title: req.body.title,
+        date: new Date().toISOString().split('T')[0],
+        excerpt: req.body.excerpt,
+        content: req.body.content
+    }
+    
+    posts.push(newPost);
+
+    fs.writeFileSync('./data/posts.json', JSON.stringify(posts, null, 2));
+
+    res.redirect("/");
+});
+
+// converts the title into a slug
+function slugGenerator(title) {
+    let slug = title.toLowerCase().replaceAll(" ", "-");
+    return slug;
+}
+
+function getDate() {
+    let d = new Date();
+    let currentDate = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`;
+    return currentDate;
+}
+
+// listens to the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
